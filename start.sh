dce085da0fa1cc1a9a588ccda61271ca353c8233
josephbienvenu <josephbienvenuowonoabega@outlook.com>
2025-06-09 16:19:51 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+# SET non-interactive for timezone
+export DEBIAN_FRONTEND=noninteractive
+ln -fs /usr/share/zoneinfo/Asia/Kathmandu  /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
+
+# Install tmate and expect
+apt-get update
+apt-get install -y tmate expect
+
+# Print SSH and Web (read-write) session links
+echo "SSH access:"
+tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+
+# Auto type to keep it alive
+while true; do
+    tmate -S /tmp/tmate.sock send-keys "echo alive && date" C-m
+    sleep 300
+done
